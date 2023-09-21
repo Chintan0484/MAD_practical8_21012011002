fa064423adc9a707651f4ab5f80cad30d61544d9
student <student@gnu.ac.in>
2023-09-21 08:40:42 +0530

    pr8

--- app/src/main/java/com/example/mad_practical8_21012011002/MainActivity.kt
+++ app/src/main/java/com/example/mad_practical8_21012011002/MainActivity.kt
@@ -2,10 +2,20 @@ package com.example.mad_practical8_21012011002
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import com.google.android.material.card.MaterialCardView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val card=findViewById<MaterialCardView>(R.id.card_cancle)
+        card.visibility=View.GONE
+        val alarmbutton=findViewById<Button>(R.id.clock)
+        alarmbutton.setOnClickListener {
+            card.visibility=View.VISIBLE
+        }
+
     }
 }
